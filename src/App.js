df3597a5e1494097918d6656a5aa5d21bfb54fcd
unknown <mikejobapplicant97@gmail.com>
2024-12-12 00:12:04 -0500

    add service section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import HeroSection from "./components/HeroSection";
+import ServiceSection from "./components/ServiceSection";
 
 function App() {
   return (
     <div>
       <Navbar />
       <HeroSection />
+      <ServiceSection />
     </div>
   );
 }
---
+++ src/components/ServiceSection.js
@@ -0,0 +1,11 @@
+const ServiceSection = () => {
+  return (
+    <div className="w-full md:w-1/2 lg:w-1/3 px-4">
+      <h1>Vision</h1>
+      <h1>Mission</h1>
+      <h1>Philosophy</h1>
+    </div>
+  );
+};
+
+export default ServiceSection;
