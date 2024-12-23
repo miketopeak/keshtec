7a76b2a3174a9495ac6a03c17d1faaf33de8b668
unknown <mikejobapplicant97@gmail.com>
2024-12-23 10:15:10 -0500

    added icon to service goal section card

--- src/components/ServiceGoalSection.js
+++ src/components/ServiceGoalSection.js
@@ -1,10 +1,19 @@
+import { HiMiniCalculator } from "react-icons/hi2";
+import { AiOutlineBarChart } from "react-icons/ai";
+import { GiBrain } from "react-icons/gi";
+
 const ServiceGoalSection = () => {
   return (
     <div className="auto-container">
       <div className="flex flex-wrap -mx-4">
         <div className="w-full md:w-1/2 lg:w-1/3 mb-4 ">
-          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg">
-            <h1 className="text-2xl font-bold py-[18px]">Vision</h1>
+          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg relative">
+            <div className="absolute -top-14 left-1/2 transform -translate-x-1/2">
+              <div className="w-[118px] h-[118px] rounded-full flex items-center justify-center bg-gradient-to-b from-[#45A401] to-[#9EE213]">
+                <HiMiniCalculator  className="text-white text-6xl" />
+              </div>
+            </div>
+            <h1 className="text-2xl font-bold py-[18px] mt-[70px]">Vision</h1>
             <div className="text-base text-[#1a1e66] flex-grow">
               We create consistently and develop as a significant IT provider to
               become the main performer in giving quality Web and Software
@@ -18,8 +27,13 @@ const ServiceGoalSection = () => {
           </div>
         </div>
         <div className="w-full md:w-1/2 lg:w-1/3 mb-4 ">
-          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg">
-            <h1 className="text-2xl font-bold py-[18px]">Mission</h1>
+          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg relative">
+          <div className="absolute -top-14 left-1/2 transform -translate-x-1/2">
+              <div className="w-[118px] h-[118px] rounded-full flex items-center justify-center bg-gradient-to-b from-[#45A401] to-[#9EE213]">
+                <AiOutlineBarChart  className="text-white text-6xl" />
+              </div>
+            </div>
+            <h1 className="text-2xl font-bold py-[18px] mt-[70px]">Mission</h1>
             <div className="text-base text-[#1a1e66] flex-grow">
               We enhance the business development of our clients with innovative
               Design and Development to convey market-defining top high-quality
@@ -33,8 +47,13 @@ const ServiceGoalSection = () => {
           </div>
         </div>
         <div className="w-full md:w-1/2 lg:w-1/3 mb-4 ">
-          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg">
-            <h1 className="text-2xl font-bold py-[18px]">Philosophy</h1>
+          <div className="service-box flex flex-col h-full bg-white p-6 shadow-lg rounded-lg relative">
+          <div className="absolute -top-14 left-1/2 transform -translate-x-1/2">
+              <div className="w-[118px] h-[118px] rounded-full flex items-center justify-center bg-gradient-to-b from-[#45A401] to-[#9EE213]">
+                <GiBrain  className="text-white text-6xl" />
+              </div>
+            </div>
+            <h1 className="text-2xl font-bold py-[18px] mt-[70px]">Philosophy</h1>
             <div className="text-base text-[#1a1e66] flex-grow">
               Our way to deal with business and programming improvement is
               relatively simple and adaptable enough to support any client,
